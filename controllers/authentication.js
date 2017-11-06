11387f2c0f85fa1475282591d3e5e6e64e86f8e5
Nina Louw <ninalouw@mac.com>
2017-11-06 09:05:10 -0800

    encrypt password with bcrypt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,6 +3,9 @@ const User = require('../models/user');
 exports.signup = function(req,res,next){
     const email = req.body.email;
     const password = req.body.password;
+    if(!email || !password){
+        return res.status(422).send({error: 'You must provide an email and password'});
+    }
     //see if user with given email exists
     User.findOne({ email : email }, function(err, existingUser){
         if(err) { return next(err);}
