1b622bbc60f0543896d218771d509c607a5df238
Nina Louw <ninalouw@mac.com>
2017-11-14 08:41:37 -0800

    start implementing local strategy

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,15 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+//create local strategy
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function(email, password){
+    //verify tis username and pw, call done with user
+    //if it is the correct username and pw
+    //otherwise, call done with false
+});
 
 //setup options for JWT Strategy
 const jwtOptions = {
